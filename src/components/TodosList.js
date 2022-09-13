1deb0b50af57025ab4c80d63125f4f8b5926a360
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-13 16:05:47 +0100

    todolist

---
+++ src/components/TodosList.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from "react"
+import ReactDOM from "react-dom"
+class TodosList extends React.PureComponent{
+    render(){
+        return(
+        <h1>Hello world</h1>
+        );
+    }
+}
+export default TodosList;
